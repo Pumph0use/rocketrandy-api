e4bf508960c94d7815d0d8427b6bbe8588bc7b04
Dustin Sims <dustinsims0@gmail.com>
2021-04-30 12:58:36 -0500

    FIX once more with gusto because I can't type

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="app",
-    packages=["ap"],
+    packages=["app"],
     include_package_data=True,
     install_requires=["flask", "flask-sqlalchemy", "flask-migrate", "flask-cors", "psycopg2", "python-dotenv"],
 )
