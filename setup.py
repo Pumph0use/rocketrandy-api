520bb65b3be21019366b844ee037373b5389fd01
Dustin Sims <dustinsims0@gmail.com>
2021-04-30 12:56:09 -0500

    FIX package name got changed by refactor

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="bot",
-    packages=["bot"],
+    name="api",
+    packages=["api"],
     include_package_data=True,
     install_requires=["flask", "flask-sqlalchemy", "flask-migrate", "flask-cors", "psycopg2", "python-dotenv"],
 )
